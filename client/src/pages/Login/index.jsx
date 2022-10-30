bc8ee0bf15df2241a826558ac28b3c18a7139238
vy3004 <you@example.comvytrannguyen3004@gmail.com>
2022-10-31 00:20:14 +0700

    update client login, register, re password

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Input, Row, Tabs } from "antd";
 import { LockOutlined, MobileOutlined } from "@ant-design/icons";
 import ReactPhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
-import { Link } from "react-router-dom";
+import QRCode from "qrcode.react";
+import { Link, useNavigate } from "react-router-dom";
 import "./style.css";
+import AuthOtp from "../AuthOtp";
 
 export default function Login() {
+  const [checkOtp, setCheckOtp] = useState(true);
+  const navigate = useNavigate();
+
+  const handleAuthOtp = () => {
+    setCheckOtp(!checkOtp);
+  };
+
+  const handleLogin = () => {
+    navigate("/");
+  };
+
   return (
     <div className="wrapper-login">
       <Row>
@@ -17,51 +30,84 @@ export default function Login() {
           </span>
         </Col>
         <Col className="login-body" span={24}>
-          <Tabs defaultActiveKey="2" className="tabs" centered>
-            <Tabs.TabPane tab="VỚI MÃ QR" key="1">
-              <span>Coming soon</span>
-            </Tabs.TabPane>
-            <Tabs.TabPane
-              tab="VỚI SỐ ĐIỆN THOẠI"
-              key="2"
-              className="login-from"
-            >
-              <div className="login-phone-input">
-                <MobileOutlined
-                  style={{
-                    fontSize: "17px",
-                    padding: "0 11px",
-                  }}
-                />
-                <ReactPhoneInput
-                  className="phone-input"
-                  country={"vn"}
-                  placeholder="Số điện thoại"
-                  autoFormat
-                  disableCountryCode
-                  inputStyle={{ border: "none" }}
-                  buttonStyle={{ border: "none", backgroundColor: "white" }}
-                ></ReactPhoneInput>
-              </div>
-              <br></br>
-              <div className="login-password-input">
-                <Input.Password
-                  size="large"
-                  bordered={false}
-                  placeholder="Mật khẩu"
-                  prefix={<LockOutlined />}
-                />
-              </div>
-              <Button className="login-btn-login">
-                Đăng nhập với mật khẩu
-              </Button>
-              <Button className="login-btn-sender">
-                Gửi yêu cầu đăng nhập
-              </Button>
+          <div style={{ display: checkOtp ? "block" : "none" }}>
+            <Tabs
+              defaultActiveKey="2"
+              centered
+              items={[
+                {
+                  label: `VỚI MÃ QR`,
+                  key: "1",
+                  children: (
+                    <div className="login-qr">
+                      <QRCode
+                        className="qr-code"
+                        value="Để cho zui"
+                        size={250}
+                        level={"H"}
+                        includeMargin={true}
+                      />
+                      <span>Quét mã QR bằng Yalo để đăng nhập</span>
+                    </div>
+                  ),
+                },
+                {
+                  label: `VỚI SỐ ĐIỆN THOẠI`,
+                  key: "2",
+                  children: (
+                    <form className="login-form">
+                      <div className="login-phone-input">
+                        <MobileOutlined
+                          style={{
+                            fontSize: "17px",
+                            padding: "0 11px",
+                          }}
+                        />
+                        <ReactPhoneInput
+                          className="phone-input"
+                          country={"vn"}
+                          placeholder="Số điện thoại"
+                          autoFormat
+                          disableCountryCode
+                          inputStyle={{ border: "none" }}
+                          buttonStyle={{
+                            border: "none",
+                            backgroundColor: "white",
+                          }}
+                        ></ReactPhoneInput>
+                      </div>
+                      <br></br>
+                      <div className="login-password-input">
+                        <Input.Password
+                          size="large"
+                          bordered={false}
+                          placeholder="Mật khẩu"
+                          prefix={<LockOutlined />}
+                        />
+                      </div>
+                      <Button className="login-btn-login" onClick={handleLogin}>
+                        Đăng nhập với mật khẩu
+                      </Button>
+                      <Button
+                        className="login-btn-sender"
+                        onClick={handleAuthOtp}
+                      >
+                        Gửi yêu cầu đăng nhập
+                      </Button>
 
-              <Link className="login-link">Quên mật khẩu?</Link>
-            </Tabs.TabPane>
-          </Tabs>
+                      <Link className="login-link" to="/repassword">
+                        Quên mật khẩu?
+                      </Link>
+                    </form>
+                  ),
+                },
+              ]}
+            />
+          </div>
+
+          <div style={{ display: !checkOtp ? "block" : "none" }}>
+            <AuthOtp />
+          </div>
         </Col>
         <Col className="login-footer" span={24}>
           <span>
